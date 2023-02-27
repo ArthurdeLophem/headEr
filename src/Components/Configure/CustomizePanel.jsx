bcaedb4e776bec736413c66b0a69ba2b7eabc624
r0808 <arthurdelophem3@gmail.com>
2023-02-27 11:35:06 +0100

    feat: can see file input if active hp part equals drivers

--- src/Components/Configure/CustomizePanel.jsx
+++ src/Components/Configure/CustomizePanel.jsx
@@ -4,6 +4,15 @@ export default function CustomizePanel(props) {
     const handleOut = () => {
         props.chooseActiveEl("hidden")
     }
+
+    let drivers = false
+    if (props.activeEl == "drivers") {
+        drivers = true
+        console.log('true')
+    } else {
+        drivers = false
+    }
+
     return <>
         <div className={`panel ${props.activeEl}`}>
             <div className={`w-full flex justify-between`}>
@@ -11,6 +20,20 @@ export default function CustomizePanel(props) {
                 <p onClick={handleOut} className=" font-bold cursor-pointer">X</p>
             </div>
             <RgbaColorPicker onChange={props.chooseColor} className="picker mb-5" />
+
+            {drivers && (
+                <div className="flex gap-4">
+                    <label htmlFor="company_pic">Choose file to upload</label>
+                    <input
+                        className="w-52"
+                        type="file"
+                        id="company_pic"
+                        name="company_pic"
+                        accept=".jpg, .jpeg, .png, svg" />
+                </div>
+            )}
+
         </div>
+
     </>
 }
