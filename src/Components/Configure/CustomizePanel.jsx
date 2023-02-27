5eb8667cf5ac03027370470933d904407b22f568
r0808 <arthurdelophem3@gmail.com>
2023-02-27 12:12:29 +0100

    feat: can add a picture and get preview in panel

--- src/Components/Configure/CustomizePanel.jsx
+++ src/Components/Configure/CustomizePanel.jsx
@@ -1,6 +1,21 @@
 import { RgbaColorPicker } from "react-colorful";
+import { useRef, useEffect, useState } from 'react';
 
 export default function CustomizePanel(props) {
+    const img = useRef()
+    const [compimg, setCompimg] = useState(null);
+
+    const handleImage = (e) => {
+        const files = e.target.files[0];
+        if (files) {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(files);
+            fileReader.addEventListener("load", function () {
+                setCompimg(`${this.result}`)
+            });
+        }
+    }
+
     const handleOut = () => {
         props.chooseActiveEl("hidden")
     }
@@ -8,7 +23,6 @@ export default function CustomizePanel(props) {
     let drivers = false
     if (props.activeEl == "drivers") {
         drivers = true
-        console.log('true')
     } else {
         drivers = false
     }
@@ -22,14 +36,17 @@ export default function CustomizePanel(props) {
             <RgbaColorPicker onChange={props.chooseColor} className="picker mb-5" />
 
             {drivers && (
-                <div className="flex gap-4">
-                    <label htmlFor="company_pic">Choose file to upload</label>
-                    <input
-                        className="w-52"
-                        type="file"
-                        id="company_pic"
-                        name="company_pic"
-                        accept=".jpg, .jpeg, .png, svg" />
+                <div className="flex flex-col items-center gap-8 mb-4">
+                    <div className="flex gap-4">
+                        <label htmlFor="company_pic">Choose file to upload</label>
+                        <input onChange={handleImage}
+                            className="w-52"
+                            type="file"
+                            id="company_pic"
+                            name="company_pic"
+                            accept=".jpg, .jpeg, .png, svg" />
+                    </div>
+                    {compimg !== null && (<img src={compimg} alt="" />)}
                 </div>
             )}
 
